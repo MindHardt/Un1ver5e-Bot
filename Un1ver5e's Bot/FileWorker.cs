a4473397c4032b6755c6959ee44df973b83a8521
MindHardt <71509337+MindHardt@users.noreply.github.com>
2021-09-01 22:49:52 +0500

    Добавьте файлы проекта.

---
+++ Un1ver5e's Bot/FileWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Un1ver5e.FileWorker
+{
+    public static class FileWorker
+    {
+        public interface IСsvStorable
+        {
+            public string CsvStore();
+            public object[] CsvGet();
+        }
+
+    }
+}
---
+++ Un1ver5e's Bot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace Un1ver5e.Bot
+{
+    public static class Program
+    {
+        //<Global parameters>
+
+        //The main Random object, all the code is recommended to use this one
+        public readonly static Random Randomizer = new Random();
+
+        //The main DiscordClient object, all the code is recommended to use this one
+        public static DiscordClient Client { get; private set; }
+
+        //</Global parameters>
+
+        //The underlying line is required for the bot to run asyncronously
+        private static void Main(string[] args) => MainTask(args).ConfigureAwait(false).GetAwaiter().GetResult();
+
+        //The underlying block is the Main code
+        private static async Task MainTask(string[] args)
+        {
+            Client = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "ss",
+                TokenType = TokenType.Bot
+            }
+            );
+        }
+    }
+}
